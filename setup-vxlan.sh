ed6198a7cfbef306fb8700d038d960e355fcf287
Murat Magomedov <muratmagomedov@Murats-MacBook-Pro.local>
2025-02-05 15:05:13 -0800

    clean-up IP to  NIC mappings

--- setup-vxlan.sh
+++ setup-vxlan.sh
@@ -12,15 +12,16 @@ docker network create --driver bridge \
 
 # Create 8 separate networks, each with dedicated NIC
 for i in $(seq 0 7); do
-   # Define interface and IPs
-   iface="enp$((13 + i))s0np0"
-   local_ip=$(ip addr show $iface | grep -Po "inet \K10.0.$((i + 1))\.[0-9]+")
-   remote_ip1="10.0.$((i + 1)).64"  # Node 2
-   remote_ip2="10.0.$((i + 1)).48"  # Node 3
-   remote_ip3="10.0.$((i + 1)).58"  # Node 4
-   vni=$((100 + i))
-   dstport=$((4789 + i))
-   
+    # Define subnet and IPs
+    subnet="10.0.$((i + 1))"
+    local_ip="$subnet.44"    # Node 1
+    iface=$(ip -br addr show | grep "$local_ip" | awk '{print $1}') # Get NIC name
+    remote_ip1="$subnet.64"  # Node 2
+    remote_ip2="$subnet.48"  # Node 3
+    remote_ip3="$subnet.58"  # Node 4
+    vni=$((100 + i))
+    dstport=$((4789 + i))
+       
    # Create docker network with dedicated bridge
    docker network create --driver bridge \
        --opt com.docker.network.driver.mtu=4150 \
