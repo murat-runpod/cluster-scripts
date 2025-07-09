bee08d3e1dbb08eba21055a0b2e7f21613b2c263
Murat Magomedov <murat.magomedov@runpod.io>
2025-07-09 14:53:19 -0700

    combine connect + create networks

--- create_ipvlan.sh
+++ create_ipvlan.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <LAST_OCTET>"
+    exit 1
+fi
+LAST_OCTET=$1
+
 for i in {0..7} ; do
 	docker network create -d ipvlan --subnet 10.66.${i}.0/24 --label "multi-node" -o parent=enp$((i+13))s0np0 ipvlan-net-${i};
+	docker network connect --ip 10.66.${i}.${LAST_OCTET} ipvlan-net-${i} safe_runpod_node;
 done;
--- create_macvlan.sh
+++ create_macvlan.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <LAST_OCTET>"
+    exit 1
+fi
+LAST_OCTET=$1
+
 for i in {0..7} ; do
 	docker network create -d macvlan --subnet 10.66.${i}.0/24 --label "multi-node" -o parent=enp$((i+13))s0np0 macvlan-net-${i};
+	docker network connect --ip 10.66.${i}.${LAST_OCTET} macvlan-net-${i} safe_runpod_node;
 done;
