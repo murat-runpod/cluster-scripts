24e7b2ffaaec4931ad7bd118a70946dfa9a46fad
Murat Magomedov <murat.magomedov@runpod.io>
2025-07-24 17:14:32 -0700

    mtu and octets

--- create_vxlan.sh
+++ create_vxlan.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 <LOCAL> <REMOTE>"
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 <LOCAL> <REMOTE> <OCTET>"
     exit 1
 fi
 
 LOCAL=$1
 REMOTE=$2
+OCTET=$3
 
-sudo ip link add vxlan100 type vxlan id 100 dstport 4789 local 10.0.6.${LOCAL} remote 10.0.6.${REMOTE} # dev enp13s0np0
-sudo ip addr add 192.168.100.1/24 dev vxlan100
-sudo ip link set vxlan100 up
+ip link add vxlan100 type vxlan id 100 dstport 4789 local 10.0.6.${LOCAL} remote 10.0.6.${REMOTE} # dev enp13s0np0
+ip addr add 192.168.100.${OCTET}/24 dev vxlan100
+ip link set dev vxlan100 mtu 4200
+ip link set vxlan100 up
 ip addr show vxlan100
