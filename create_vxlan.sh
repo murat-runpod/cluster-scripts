447179e5b897e91b21edecaa4533bac691e617eb
Murat Magomedov <murat.magomedov@runpod.io>
2025-07-29 16:42:40 -0700

    cidr block

--- create_vxlan.sh
+++ create_vxlan.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 <LOCAL> <REMOTE>"
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <LAST_OCTET>"
     exit 1
 fi
 
 LOCAL=$1
-REMOTE=$2
 
 SUBNETS=(6 7 5 8 1 2 3 4)
 
@@ -14,8 +13,8 @@ for i in {0..7} ; do
     VNI=$((100 + i))
     VXLAN_NAME="vxlan${i}"
 
-    ip link add ${VXLAN_NAME} type vxlan id ${VNI} dstport 4789 local 10.0.${LINK}.${LOCAL} remote 10.0.${LINK}.${REMOTE}
-    ip addr add 192.168.${i}.${LOCAL}/24 dev ${VXLAN_NAME}
+    ip link add ${VXLAN_NAME} type vxlan id ${VNI} dstport 4789
+    ip addr add 10.200.${i}.${LOCAL}/24 dev ${VXLAN_NAME}
     ip link set dev ${VXLAN_NAME} mtu 4150
     ip link set ${VXLAN_NAME} up
     ip -c -4 addr show ${VXLAN_NAME}
