d7af8ea8e7b8b8ca69cdd42f8dbd049539d27629
Murat Magomedov <murat.magomedov@runpod.io>
2025-07-29 17:36:32 -0700

    add dev and group

--- create_vxlan.sh
+++ create_vxlan.sh
@@ -13,7 +13,7 @@ for i in {0..7} ; do
     VNI=$((100 + i))
     VXLAN_NAME="vxlan${i}"
 
-    ip link add ${VXLAN_NAME} type vxlan id ${VNI} dstport 4789
+    ip link add ${VXLAN_NAME} type vxlan id ${VNI} group 239.0.0.10 dstport 4789 dev enp$((13+i))s0np0
     ip addr add 10.200.${i}.${LOCAL}/24 dev ${VXLAN_NAME}
     ip link set dev ${VXLAN_NAME} mtu 4150
     ip link set ${VXLAN_NAME} up
